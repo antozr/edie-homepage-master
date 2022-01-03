5012c7afd0bb0becbbc0ec7ac3c67fefcaff56a6
anto__dev <anto__dev@users.noreply.github.com>
2022-01-03 02:09:36 +0100

    Add the mobile design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
 "use stricts"; 
 
 console.log("hello ! ");
+
+var subEmailHero = document.getElementById('subEmailHero');
+var email = document.getElementById('email');
+
+email.addEventListener('click', ()=>{
+    console.log("email!");
+    subEmailHero.classList.add('cont__subInput--animation');
+});
+
+subEmailHero.addEventListener('click', ()=>{
+    subEmailHero.classList.remove('cont__subInput--animation');
+});
+
+
+var subEmailHero2 = document.getElementById('subEmailHero2');
+var emai2 = document.getElementById('email2');
+
+email2.addEventListener('click', ()=>{
+    console.log("email!");
+    subEmailHero2.classList.add('cont__subInput--animation');
+})
+
+subEmailHero2.addEventListener('click', ()=>{
+    subEmailHero2.classList.remove('cont__subInput--animation');
+})
