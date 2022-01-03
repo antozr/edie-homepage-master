2e92c37c1590143cec23fa17bfefa5a471a3110d
anto__dev <anto__dev@users.noreply.github.com>
2022-01-03 15:45:27 +0100

    add mobile menu with animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,4 +25,43 @@ email2.addEventListener('click', ()=>{
 
 subEmailHero2.addEventListener('click', ()=>{
     subEmailHero2.classList.remove('cont__subInput--animation');
+});
+
+
+//// the button of menu 
+
+var btnMenu = document.querySelector('.head__menu');
+var head__menuBar = document.querySelectorAll('.head__menuBar');
+var head__nav = document.querySelector('.head__nav');
+var nav__lien = document.querySelectorAll('.nav__lien');
+
+
+btnMenu.addEventListener('click', ()=>{
+    
+    if(btnMenu.classList.contains('head__menu--click')){
+        
+        btnMenu.classList.remove('head__menu--click');
+        head__nav.classList.remove('head__nav--click');
+        head__menuBar.forEach(el =>{
+        el.classList.remove('head__menuBar--click');
+    });
+    }else{
+        btnMenu.classList.add('head__menu--click');
+        head__nav.classList.add('head__nav--click');
+        head__menuBar.forEach(el =>{
+            el.classList.add('head__menuBar--click');
+        })
+    }
+    
+    
+});
+
+nav__lien.forEach(link =>{
+    link.addEventListener('click', ()=>{
+        btnMenu.classList.remove('head__menu--click');
+        head__nav.classList.remove('head__nav--click');
+        head__menuBar.forEach(el =>{
+        el.classList.remove('head__menuBar--click');
+    })
+    })
 })
